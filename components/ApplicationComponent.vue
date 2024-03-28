de9e0a393e8db0d1c9e2c202d8faa8a616363fe7
Gerhard Seidel <gseidel.message@googlemail.com>
2024-03-28 12:00:32 +0800

    chore: change to vue composition api (#1961)
    | Q            | A
    |--------------| ---
    | Bug fix?     | no
    | New feature? | no
    | BC-Break?    | no
    | License      | MIT
    
    * Change from vue class component to vue composition api, because class component is deprecated and composition api is widly used

--- components/ApplicationComponent.vue
+++ components/ApplicationComponent.vue
@@ -11,22 +11,13 @@
     </div>
 </template>
 
-<script lang="ts">
-import {Inject, Options, Vue} from "vue-property-decorator";
+<script setup lang="ts">
+import {inject} from "vue";
 import '@enhavo/app/assets/fonts/enhavo-icons.font'
 import '@enhavo/app/assets/styles/view.scss';
-import DashboardApp from "@enhavo/dashboard/dashboard/DashboardApp";
 import WidgetManager from "@enhavo/dashboard/widget/WidgetManager";
 
-@Options({})
-export default class ApplicationComponent extends Vue
-{
-    @Inject()
-    dashboard: DashboardApp
-
-    @Inject()
-    widgetManager: WidgetManager
-}
+const widgetManager = inject<WidgetManager>('widgetManager');
 </script>
 
 <style lang="css">
