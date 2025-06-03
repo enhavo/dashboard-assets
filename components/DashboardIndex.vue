55cab91d32d8664d5011ca93cb86e164f9db7153
Gerhard Seidel <gs@weareindeed.com>
2025-06-03 09:39:55 +0800

    chore: yarn workspace (#2269)
    | Q            | A
    |--------------| ---
    | Bug fix?     | no
    | New feature? | no
    | License      | MIT
    
    Use yarn workspaces instead of `assets/node_modules` folder. The `node_modules` folder has some disadvantages, as the name is often use to ignore processes inside this folder e.g. watching files, code analysis in ci tools etc. Yarn workspaces is made for mono repositories, so we decide with this solution, because yarn is already used as our package manager.

---
+++ components/DashboardIndex.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="app-view">
+        <div class="dashboard-widgets">
+            <template v-if="dashboardManager.widgets" v-for="widget of dashboardManager.widgets">
+                <component :is="widget.component" :widget="widget"></component>
+            </template>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import {inject} from "vue";
+import '@enhavo/app/assets/styles/view.scss';
+import '@enhavo/dashboard/assets/styles/dashboard.scss';
+import {DashboardManager} from "@enhavo/dashboard/manager/DashboardManager";
+
+const dashboardManager = inject<DashboardManager>('dashboardManager');
+dashboardManager.load()
+</script>
+
+<style lang="css">
+.app-view { height: 100vh }
+</style>
---
+++ components/DashboardWidgetNumber.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="dashboard-widget"
+         :class="'width-' + widget.width"
+    >
+        <div class="widget-content">
+            <div class="label">{{ widget.label }}</div>
+            <div class="value">{{ widget.value }}</div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import {NumberDashboardWidget} from "@enhavo/dashboard/dashboard/model/NumberDashboardWidget";
+
+const props = defineProps<{
+    widget: NumberDashboardWidget
+}>()
+</script>
